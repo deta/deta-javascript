930e845e7ee9f997221b1ee3d7158b92f449e258
Rabin Gaire <rabingaire20@gmail.com>
2021-05-14 17:52:09 +0545

    read projectkey from env

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import DetaClass from './deta';
 /**
  * Deta returns instance of Deta class
  *
- * @param {string} projectKey
+ * @param {string} [projectKey]
  * @returns {DetaClass}
  */
-export function Deta(projectKey: string): DetaClass {
-  const key = projectKey.trim();
+export function Deta(projectKey?: string): DetaClass {
+  const key = projectKey?.trim() || process.env.DETA_PROJECT_KEY?.trim();
   if (!key) {
     throw new Error('Project key is not defined');
   }
