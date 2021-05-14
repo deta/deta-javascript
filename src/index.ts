cd5bfbaa6eb8493d2fafb58c8031f811da877944
Rabin Gaire <rabingaire20@gmail.com>
2021-05-14 19:12:27 +0545

    expose base and drive methods on deta sdk

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import DetaClass from './deta';
+import BaseClass from './base';
+import DriveClass from './drive';
 
 /**
  * Deta returns instance of Deta class
@@ -12,4 +14,24 @@ export function Deta(projectKey?: string): DetaClass {
     throw new Error('Project key is not defined');
   }
   return new DetaClass(key);
+}
+
+/**
+ * Base returns instance of Base class
+ *
+ * @param {string} baseName
+ * @returns {BaseClass}
+ */
+export function Base(baseName: string): BaseClass {
+  return Deta().Base(baseName);
+}
+
+/**
+ * Drive returns instance of Drive class
+ *
+ * @param {string} driveName
+ * @returns {DriveClass}
+ */
+export function Drive(driveName: string): DriveClass {
+  return Deta().Drive(driveName);
 }
