ed242917c0a8cbf419b4962ebc773900bbf32819
Rabin Gaire <rabingaire20@gmail.com>
2021-05-14 18:01:16 +0545

    read baseURL from env

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,7 @@
+const base = process.env.DETA_BASE_HOST?.trim() || 'database.deta.sh/v1';
+const drive = process.env.DETA_DRIVE_HOST?.trim() || 'drive-staging.deta.sh/v1';
+
 export default {
-  BASE_HOST_URL: 'https://database.deta.sh/v1/:project_id/:base_name',
-  DRIVE_HOST_URL: 'https://drive-staging.deta.sh/v1/:project_id/:drive_name',
+  BASE_HOST_URL: `https://${base}/:project_id/:base_name`,
+  DRIVE_HOST_URL: `https://${drive}/:project_id/:drive_name`,
 };
