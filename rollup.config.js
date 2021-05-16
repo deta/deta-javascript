9c4929a35d710b2c3484d1e21419a8db131458b3
Rabin Gaire <rabingaire20@gmail.com>
2021-05-17 00:24:23 +0545

    update configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,25 @@
+import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
+import pkg from './package.json';
+
 export default {
   input: './src/index.ts',
-  output: {
-    file: './build/index.js',
-    format: 'cjs',
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs', // commonJS
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'esm', // ES Modules
+      sourcemap: true,
+    },
+  ],
   plugins: [
     typescript({
       tsconfig: './tsconfig.json',
@@ -17,5 +27,6 @@ export default {
     commonjs({ extensions: ['.ts'] }),
     nodeResolve(),
     sourceMaps(),
+    terser(),
   ],
 };
