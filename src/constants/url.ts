6d3fa4d95adc4c3655038f1867d6e837490b973d
Rabin Gaire <rabingaire20@gmail.com>
2021-05-19 01:22:28 +0545

    fix drive base url

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,7 +1,7 @@
 const base = process.env.DETA_BASE_HOST?.trim() || 'database.deta.sh/v1';
-const drive = process.env.DETA_DRIVE_HOST?.trim() || 'localhost:9015/v1';
+const drive = process.env.DETA_DRIVE_HOST?.trim() || 'drive.deta.sh/v1';
 
 export default {
   BASE_HOST_URL: `https://${base}/:project_id/:base_name`,
-  DRIVE_HOST_URL: `http://${drive}/:project_id/:drive_name`,
+  DRIVE_HOST_URL: `https://${drive}/:project_id/:drive_name`,
 };
