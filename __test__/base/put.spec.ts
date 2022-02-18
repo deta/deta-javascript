c4d24d0560313b9941057dfac96b87cf5b13640a
Rabin Gaire <rabingaire20@gmail.com>
2022-02-18 19:35:08 +0545

    add ttl test case for Base#put

--- __test__/base/put.spec.ts
+++ __test__/base/put.spec.ts
@@ -1,8 +1,20 @@
 import { Base } from '../utils/deta';
+import { Day } from '../../src/utils/date';
+import { BaseGeneral } from '../../src/constants/general';
 
 const db = Base();
 
 describe('Base#put', () => {
+  beforeAll(() => {
+    jest.useFakeTimers('modern');
+    const date = new Date();
+    jest.setSystemTime(date);
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it.each([
     [
       { name: 'alex', age: 77 },
@@ -59,6 +71,48 @@ describe('Base#put', () => {
       expect(data).toEqual(expected);
       const deleteRes = await db.delete(data?.key as string);
       expect(deleteRes).toBeNull();
+    }
+  );
+
+  it.each([
+    [
+      { name: 'alex', age: 77 },
+      'put_two',
+      { expireIn: 5 },
+      {
+        name: 'alex',
+        age: 77,
+        key: 'put_two',
+        [BaseGeneral.TTL_ATTRIBUTE]: new Day().addSeconds(5).getEpochSeconds(),
+      },
+    ],
+    [
+      'hello, worlds',
+      'put_three',
+      { expireAt: new Date() },
+      {
+        value: 'hello, worlds',
+        key: 'put_three',
+        [BaseGeneral.TTL_ATTRIBUTE]: new Day().getEpochSeconds(),
+      },
+    ],
+    [
+      ['a', 'b', 'c'],
+      'put_my_abc',
+      { expireIn: 5, expireAt: new Date() },
+      new Error("can't set both expireIn and expireAt options"),
+    ],
+  ])(
+    'by passing data as first parameter, key as second parameter and options as third parameter `put(%p, "%s", %p)`',
+    async (value, key, options, expected) => {
+      try {
+        const data = await db.put(value, key, options);
+        expect(data).toEqual(expected);
+        const deleteRes = await db.delete(data?.key as string);
+        expect(deleteRes).toBeNull();
+      } catch (err) {
+        expect(err).toEqual(expected);
+      }
     }
   );
 });
