eee1b8b93c0aefe61de858b9006ccc4c6b6d7e50
BetaHuhn <schiller@mxis.ch>
2023-03-02 11:57:45 +0100

    breaking-change: remove detalib

--- src/index.node.ts
+++ src/index.node.ts
@@ -1,23 +1,9 @@
 import fetch from 'node-fetch';
-import { DetaLib, DetaLibApp } from './types/libjs';
 
 // fetch polyfill for nodejs
 if (!globalThis.fetch) {
   // @ts-ignore
   globalThis.fetch = fetch;
 }
-
-let app: DetaLib; // eslint-disable-line import/no-mutable-exports
-let App: DetaLibApp; // eslint-disable-line import/no-mutable-exports
-
-try {
-  const { App: lib } = require('detalib');
-  app = lib();
-  App = lib;
-} catch {
-  // ignore error
-}
-
-export { app, App };
 
 export * from './index';
